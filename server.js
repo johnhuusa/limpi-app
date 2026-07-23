87f628b721886222db4a541c099febf5c111b4e3
John <john.hu.usa@gmail.com>
2026-07-22 22:41:32 -0500

    Use dynamic port for Railway deployment

--- server.js
+++ server.js
@@ -153,4 +153,5 @@ app.post('/requests/:id/photo', upload.single('photo'), (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
